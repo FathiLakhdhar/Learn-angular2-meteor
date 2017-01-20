5f8a2b747ed9a1859c98a5398604762176ffddc8
Fathi Lakhdhar <Fathi Lakhdhar>
2017-01-20 19:13:03 +0100

    rsvp

--- client/imports/app/shared/rsvp.pipe.ts
+++ client/imports/app/shared/rsvp.pipe.ts
@@ -1,22 +1,20 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {Party} from '../../../../both/models/party.model';
-import {Parties} from '../../../../both/collections/parties.collection';
-import {ObservableCursor} from 'meteor-rxjs';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Party } from '../../../../both/models/party.model';
 
 @Pipe({
     name: 'rsvp'
 })
 
-export class RsvpPipe implements PipeTransform{
+export class RsvpPipe implements PipeTransform {
 
-    transform(party: Party, type: string){
-        
-        if(!type) return 0;
+    transform(party: Party, type: string) {
+
+        if (!type) return 0;
         let total = 0;
         //const found = Parties.find(party._id);
-        if(party)
-            total= party.rsvps ? party.rsvps.filter(rsvp => rsvp.response === type).length : 0;
-
+        if (party) {
+            total = party.rsvps ? party.rsvps.filter(rsvp => rsvp.response === type).length : 0;
+        }
 
         return total;
     }
