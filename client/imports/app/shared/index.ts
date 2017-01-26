55d46b2bd01b9f02210ef682d4a3d819e58d173e
Fathi Lakhdhar <Fathi Lakhdhar>
2017-01-26 19:45:26 +0100

    smc.pipe

--- client/imports/app/shared/index.ts
+++ client/imports/app/shared/index.ts
@@ -1,5 +1,8 @@
 import {RsvpPipe} from "./rsvp.pipe";
+import {SMCPipe} from "./smc.pipe";
+
  
 export const SHARED_DECLARATIONS: any[] = [
-  RsvpPipe
+  RsvpPipe,
+  SMCPipe
 ];
---
+++ client/imports/app/shared/smc.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+
+@Pipe({
+    name: 'smc'
+})
+
+export class SMCPipe implements PipeTransform {
+    transform(str: String, m: String) {
+        
+        if(m.length>0){
+            var re = new RegExp(""+m, 'gi');
+            var newstr = str.replace(re, "<b class='motfind'>"+m+"</b>");
+            return newstr;
+        }
+
+        return str;
+        
+
+
+    }
+}
