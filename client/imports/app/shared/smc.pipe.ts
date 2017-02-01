42b64a8669725b05f92fe0311cf1c68c60e94d09
Fathi Lakhdhar <Fathi Lakhdhar>
2017-02-01 09:30:26 +0100

    RegEx

--- client/imports/app/shared/smc.pipe.ts
+++ client/imports/app/shared/smc.pipe.ts
@@ -10,8 +10,8 @@ export class SMCPipe implements PipeTransform {
     transform(str: String, m: String) {
         
         if(m.length>0){
-            var re = new RegExp(""+m, 'gi');
-            var newstr = str.replace(re, "<b class='motfind'>"+m+"</b>");
+            var re = new RegExp("("+m+")", 'gi');
+            var newstr = str.replace(re, "<b class='motfind'>$1</b>");
             return newstr;
         }
 
